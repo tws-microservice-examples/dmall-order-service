1f18a442985b668271ce415846a0f74ed0ce276a
zjx-immersion <jianxin_home@163.com>
2017-12-30 17:26:03 +0800

    [Jason]fixed the image build failed in jenkins

--- genImages.sh
+++ genImages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker build -t $DMALL_DOCKER_REGISTRY/dmall/order-service:$BUILD_NUMBER .
-docker tag $DMALL_DOCKER_REGISTRY/dmall/order-service:$BUILD_NUMBER $DMALL_DOCKER_REGISTRY/dmall/order-service:latest
+sudo docker build -t $DMALL_DOCKER_REGISTRY/dmall/order-service:$BUILD_NUMBER .
+sudo docker tag $DMALL_DOCKER_REGISTRY/dmall/order-service:$BUILD_NUMBER $DMALL_DOCKER_REGISTRY/dmall/order-service:latest
 
-docker push $DMALL_DOCKER_REGISTRY/dmall/order-service:$BUILD_NUMBER
-docker push $DMALL_DOCKER_REGISTRY/dmall/order-service:latest
+sudo docker push $DMALL_DOCKER_REGISTRY/dmall/order-service:$BUILD_NUMBER
+sudo docker push $DMALL_DOCKER_REGISTRY/dmall/order-service:latest
 
